f83ed7c07181399f3276abf53d9f24132b8c68dd
Pranav Kokate <pranavkokate14@gmail.com>
2024-04-20 17:31:05 +0530

    exploring the base types

---
+++ 01-starting-project/basics.ts
@@ -0,0 +1,15 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// function types, parameters
+
+
+// Primitives:
+
+let age: number = 24;
+age = 21;
+
+let userName: string;
+userName = 'Max';
+
+let booleanVal: boolean;
+booleanVal = true;
