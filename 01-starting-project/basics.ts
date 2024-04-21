1a49e4154c6a7534989f343b35c3d2405a3e0fac
Pranav Kokate <pranavkokate14@gmail.com>
2024-04-21 09:05:57 +0530

    array and object types

--- 01-starting-project/basics.ts
+++ 01-starting-project/basics.ts
@@ -13,3 +13,29 @@ userName = 'Max';
 
 let booleanVal: boolean;
 booleanVal = true;
+
+// array type
+let hobbies: string[];
+hobbies = ['sports', 'cooking', 'painting'];
+
+let scoresArr: number[];
+scoresArr = [95, 32, 21, 99];
+
+// objevt type
+// let person: any;
+
+let person: {
+    name: string,
+    age: number
+}
+
+person = {
+    name: 'Max',
+    age: 32
+}
+
+// array of objects of same kind
+let people : {
+    name: string,
+    age: number
+}[];
